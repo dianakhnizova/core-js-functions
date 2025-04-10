4b8d72a319e0e7b19991a6103d578b5051d4bee3
Diana <khnizovad@gmail.com>
2025-04-10 16:17:23 +0500

    feat: complete task - getIdGeneratorFunction(startFrom)

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -236,8 +236,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentID = startFrom;
+  return function newFuncForGenerateID() {
+    const result = currentID;
+    currentID += 1;
+    return result;
+  };
 }
 
 module.exports = {
